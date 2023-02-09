7178fb9716140d5ba83ebfb06127a8289204c9b1
Caio Ryan <74019374+prrado@users.noreply.github.com>
2023-02-09 18:11:25 -0300

    Add files via upload

--- ShopApp.java
+++ ShopApp.java
@@ -13,10 +13,25 @@ public class ShopApp {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        Costumer c1 = new Costumer();
-        c1.name = "Pinky";
-       System.out.println(c1.name);
+        double tax = 0.2;
+        double total;
+        
+       Clothing item1 = new Clothing();
+       Clothing item2 = new Clothing();
+       
+       item1.description = "Blue Jacket";
+       item1.price = 20.9;
+       item1.size = "M";
+       
+       item2.description = "Orange T-shirt";
+       item2.price = 10.5;
+       item2.size = "S";
+       
+       total = item1.price*2 + item2.price + tax;
+       
+       System.out.println(total); 
     }
     
 }
